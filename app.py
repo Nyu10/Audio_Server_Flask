166e84a75ccea7e8d812f91f8b06b9351141374b
Nehemiah Yu <43020289+Nyu10@users.noreply.github.com>
2022-11-07 23:34:03 -0500

    Finished POST and download feature

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from tinytag import TinyTag
 from jsonProcessing import tagToJson, filterTheJson
+from werkzeug.utils import secure_filename
 import json
 import os
+
+AUDIO_FILE_PATH = './Audio_Files'
+ALLOWED_EXTENSIONS = {'mp3', 'flac', 'wav', 'mp4', 'm4a'}
+
+
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['UPLOAD_FOLDER'] = AUDIO_FILE_PATH
+app.config['MAX_CONTENT_PATH'] = 10000000
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-audio_file_path = './Audio_Files'
 
 
 def filter(args, files_json):
@@ -33,15 +44,32 @@ def filter(args, files_json):
 
 def handleRequest():
     args = request.args
-    files = os.listdir(audio_file_path)
+    files = os.listdir(AUDIO_FILE_PATH)
     files_json = {}
     for file_name in files:
-        full_file_path = audio_file_path + '/' + file_name
+        full_file_path = AUDIO_FILE_PATH + '/' + file_name
         metadata = TinyTag.get(full_file_path)
         files_json[file_name] = tagToJson(metadata)
     filtered_files_json = filter(args, files_json)
     return filtered_files_json
 
+
+
+@app.route('/download')
+def download_file():
+    args = request.args
+    file_name = args.get('name')
+    print(file_name)
+    return send_from_directory(app.config["UPLOAD_FOLDER"], file_name)
+
+@app.route('/post', methods =['POST'])
+def postAudioData():
+    f = request.files['file']
+    if (not allowed_file(f.filename)):
+        return "not allowed filetype \n"
+    filename = secure_filename(f.filename)
+    f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return "file uploaded successfully \n"
 
 @app.route('/list', methods =['GET'])
 def getList():
@@ -119,5 +147,25 @@ expected: one republic
 Genre and Artist
 curl "http://localhost:5000/info?genre=Pop&albumartist=OneRepublic"
 expected: one republic
+
+'''
+
+
+'''
+POST
+curl -F "file=@TheWeekend.mp3" http://localhost:5000/post
+
+Not allowed File Type
+curl -F "file=@p3_starter_code.zip" http://localhost:5000/post
+
+'''
+
+
+'''
+GET 
+
+Download 
+
+curl "http://localhost:5000/download?name=b.mp3" --output b.mp3
 
 '''
